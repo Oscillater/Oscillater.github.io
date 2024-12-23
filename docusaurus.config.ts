97524030116f8ed735f839a064bc357e09c6a73b
Oscillater <chetianwen@163.com>
2024-12-23 20:44:55 +0800

    修改配置

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,19 +9,18 @@ const config: Config = {
   tagline: "浮沉随浪记今朝",
   favicon: "img/lingchuan.ico",
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://oscillater.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "oscillater", // Usually your GitHub org/user name.
+  projectName: "oscillater.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
